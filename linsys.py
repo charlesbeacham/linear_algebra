3b2d751a312098f5df671876374a2358686c3ea6
Charles Beacham <charles.beacham@daimlertruck.com>
2022-12-06 08:42:02 -0500

    add Parametrization class framework

--- linsys.py
+++ linsys.py
@@ -202,7 +202,22 @@ class LinearSystem(object):
         temp = ['Equation {}: {}'.format(i+1,p) for i,p in enumerate(self.planes)]
         ret += '\n'.join(temp)
         return ret
+        
+class Parametrization(object):
+    BASEPT_AND_DIR_VECTORS_MUST_BE_IN_SAME_DIM_MSG = ("The basepoint and direction vectors should all live in the same dimension.")
 
+    def __init__(self, basepoint, direction_vectors):
+    
+        self.basepoint = basepoint
+        self.direction_vectors = direction_vectors
+        self.dimension = self.basepoint.dimension
+        
+        try:
+            for v in direction_vectors:
+                assert v.dimension == self.dimension
+        
+        except AssertionError:
+            raise Exception(BASEPT_AND_DIR_VECTORS_MUST_BE_IN_SAME_DIM_MSG)
 
 class MyDecimal(Decimal):
     def is_near_zero(self, eps=1e-10):
